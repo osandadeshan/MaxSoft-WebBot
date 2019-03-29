9d666316296be3e8988ea4516ac847026474a218
Osanda Deshan <osanda.deshan@gmail.com>
2019-03-29 14:00:20 +0530

    Dev BVT.bat is updated

--- bat_files/test_runner_bats/Dev BVT.bat
+++ bat_files/test_runner_bats/Dev BVT.bat
@@ -4,7 +4,7 @@ echo Executing MaxSoft WebBot Dev Test Automation Suite.............
 cd ..
 cd ..
 
-call mvn test-compile gauge:execute -DspecsDir="specs/search.spec" -Denv="dev"
+call mvn test-compile gauge:execute -DspecsDir="specs/search_question.spec" -Denv="dev"
 echo Exit Code = %ERRORLEVEL%
 if not "%ERRORLEVEL%" == "0" exit /b
 
